8bb41842820880d86ea1d06a8b181b72553a6dcf
Stephan Vock <stephan.vock@innogames.de>
2014-03-29 06:05:45 +0100

    stage bottom

--- web/js/views/Map.js
+++ web/js/views/Map.js
@@ -12,19 +12,24 @@ define('MapView', [
 		container = new createjs.Container();
 		parent.addChild(container);
 		
-		var mapTiles = assets['map_data'][mapId].tiles;
+		var mapTiles = assets['map_data'][mapId].tiles,
+			bitmap;
 		for (var i in mapTiles) {
 			for (var j in mapTiles[i]) {
 				if (0 === mapTiles[i][j]) {
 					continue;
 				}
 
-				var bitmap = new createjs.Bitmap(assets['tile'+mapTiles[i][j]]);
-				bitmap.y = ViewConstants.HUD_HEIGHT + i * ViewConstants.MAP_TILE_SIZE;
+				bitmap = new createjs.Bitmap(assets['tile'+mapTiles[i][j]]);
+				bitmap.y = ViewConstants.HUD_HEIGHT - 50 + i * ViewConstants.MAP_TILE_SIZE;
 				bitmap.x = j * ViewConstants.MAP_TILE_SIZE;
 				container.addChild(bitmap);
 			}
 		}
+
+		bitmap = new createjs.Bitmap(assets['stage_bottom']);
+		bitmap.y = ViewConstants.HUD_HEIGHT + i * ViewConstants.MAP_TILE_SIZE + ViewConstants.MAP_TILE_SIZE;
+		container.addChild(bitmap);
 	};
 
 	Map.prototype.update = function (data) {
